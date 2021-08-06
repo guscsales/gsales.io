b794672ffae3162b003c04281ca83d5e3d824244
Gustavo Sales <gustavosalessantos@gmail.com>
2021-08-05 21:54:56 -0300

    add short link for hello world video

--- pages/[redirect].js
+++ pages/[redirect].js
@@ -1,26 +1,29 @@
 const RedirectUrlsMapper = {
-    ['node']: 'https://www.youtube.com/watch?v=q0DQEKDEFZk&list=PLFRnIPKyoBHhlXP2RQsJHuicj4e6sM0ys&index=2&ab_channel=GusQuemFala',
-    ['curso-de-ingles']: 'https://go.hotmart.com/S56903817H'
-}
+	['node']:
+		'https://www.youtube.com/watch?v=q0DQEKDEFZk&list=PLFRnIPKyoBHhlXP2RQsJHuicj4e6sM0ys&index=2&ab_channel=GusQuemFala',
+	['curso-de-ingles']: 'https://go.hotmart.com/S56903817H',
+	['ola-mundo']:
+		'https://www.youtube.com/watch?v=p8LtuO9XC7w&list=PLFRnIPKyoBHhlXP2RQsJHuicj4e6sM0ys&index=6&ab_channel=GusQuemFala',
+};
 
-function Redirect () {
-    return null;
+function Redirect() {
+	return null;
 }
 
 export const getServerSideProps = async (context) => {
-    const { params, res } =  context;
-    const externalUrl = RedirectUrlsMapper[params.redirect];
+	const { params, res } = context;
+	const externalUrl = RedirectUrlsMapper[params.redirect];
 
-    if (!externalUrl) {
-        res.end();
-    }
+	if (!externalUrl) {
+		res.end();
+	}
 
-    return {
-        redirect: {
-            destination: externalUrl,
-            permanent: true,
-        },
-    };
-}
+	return {
+		redirect: {
+			destination: externalUrl,
+			permanent: true,
+		},
+	};
+};
 
-export default Redirect
+export default Redirect;
