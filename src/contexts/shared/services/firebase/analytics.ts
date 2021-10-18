3a911dac24b78ea79dda07ba7017e85dd8ad3580
Gustavo Sales <gsales@blvd.co>
2021-10-18 17:54:16 -0300

    start analytics on load page

--- src/contexts/shared/services/firebase/analytics.ts
+++ src/contexts/shared/services/firebase/analytics.ts
@@ -1,7 +1,7 @@
 import './index';
 import { getAnalytics } from 'firebase/analytics';
 
-const analytics =
+const analytics = () =>
   process.env.NEXT_PUBLIC_ENVIRONMENT === 'production' ? getAnalytics() : null;
 
 export default analytics;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppProps } from 'next/app';
 import Header from '@contexts/shared/components/header/header';
 import Footer from '@contexts/shared/components/footer/footer';
@@ -7,14 +7,18 @@ import { SWRConfig } from 'swr';
 import http from '@contexts/shared/services/http';
 import { SkeletonTheme } from 'react-loading-skeleton';
 import resolveConfig from 'tailwindcss/resolveConfig';
+import analytics from '@contexts/shared/services/firebase/analytics';
 import tailwindConfigFile from '../../tailwind.config';
 
-import '@firebase/use-analytics';
 import '@sagebox/globals.css';
 
 const tailwindConfig = resolveConfig(tailwindConfigFile);
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    analytics();
+  }, []);
+
   return (
     <SWRConfig value={{ fetcher: http, revalidateOnFocus: false }}>
       <LanguageProvider>
