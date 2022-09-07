7471a2f4309d207ab26dd8a4470bc703d5eec21c
Gustavo Sales <gustavo@blastradius.ai>
2022-09-07 09:58:39 -0300

    fix: sitemap

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -8,14 +8,15 @@ function generateSiteMap({ posts }) {
   return `<?xml version="1.0" encoding="UTF-8"?>
      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         ${navigator
+          .filter(({ url }) => !url.includes('canal.gsales.io'))
           .map(
             ({ url }) => `<url>
             <loc>${SITE_DOMAIN}${url}</loc>
-            <lastmod>2022-01-01</lastmod>
+            <lastmod>2022-09-07</lastmod>
           </url>
           <url>
                 <loc>${SITE_DOMAIN}/pt${url}</loc>
-                <lastmod>2022-01-01</lastmod>
+                <lastmod>2022-09-07</lastmod>
             </url>
           `
           )
