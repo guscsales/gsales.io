e909a676ff1e60b795e30a3b21f9a68e6c4ba6a0
Gustavo Sales <gustavo@gsales.io>
2022-10-20 10:44:46 -0300

    feat: add new redirect link

--- src/pages/[redirect].ts
+++ src/pages/[redirect].ts
@@ -7,6 +7,8 @@ const RedirectUrlsMapper = {
     'https://www.youtube.com/watch?v=zgDlT7bzUi4&list=PLFRnIPKyoBHhlXP2RQsJHuicj4e6sM0ys&index=3',
   'package-json-docs':
     'https://docs.npmjs.com/cli/v8/configuring-npm/package-json',
+  'projetos-frontend':
+    'https://www.figma.com/file/J3p5rmvQmacpyFyyogFspN/Gus-Quem-Fala---Ideias-de-Projetos-Frontend?node-id=1%3A9',
 };
 
 function Redirect() {
