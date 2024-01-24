9f5fc4ba37aa9c40b90ded2eb9c57b8736bd83c6
Gustavo Sales <gustavo@gsales.io>
2024-01-23 21:16:25 -0300

    feat: update highlight selection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,26 @@
-import fonts from '@/libs/ui/core/fonts';
-import type { Metadata } from 'next';
-import MainHeader from '@/common/components/main-header';
-import MainFooter from '@/common/components/main-footer';
-import UserService from '@/domains/users/services/user-service';
-import CreateSession from '@/common/components/create-session';
-import CoreProvider from '@/common/providers/core-provider';
-import dynamic from 'next/dynamic';
+import fonts from "@/libs/ui/core/fonts";
+import type {Metadata} from "next";
+import MainHeader from "@/common/components/main-header";
+import MainFooter from "@/common/components/main-footer";
+import UserService from "@/domains/users/services/user-service";
+import CreateSession from "@/common/components/create-session";
+import CoreProvider from "@/common/providers/core-provider";
+import dynamic from "next/dynamic";
 
-import '@/common/styles/globals.css';
-import 'react-loading-skeleton/dist/skeleton.css';
+import "@/common/styles/globals.scss";
+import "react-loading-skeleton/dist/skeleton.css";
 
-const CommandK = dynamic(() => import('@/common/components/command-k'), {
+const CommandK = dynamic(() => import("@/common/components/command-k"), {
   ssr: false,
 });
 
 export const metadata: Metadata = {
   title: {
-    template: '%s | Gustavo Sales',
-    default: 'Software Engineer | Javascript Specialist | Gustavo Sales',
+    template: "%s | Gustavo Sales",
+    default: "Software Engineer | Javascript Specialist | Gustavo Sales",
   },
   icons: {
-    icon: '/favicon.png',
+    icon: "/favicon.png",
   },
 };
 
